b166d0ac653d2410e613373e768ea7c137dfecc0
Leandro Brotas <leandromacedo.tecnologia@gmail.com>
2019-05-13 08:55:27 -0300

    Placing pieces on the board

--- src/boardgame/Board.java
+++ src/boardgame/Board.java
@@ -9,6 +9,7 @@ public class Board {
 	public Board(int rows, int columns) {
 		this.rows = rows;
 		this.columns = columns;
+		pieces = new Piece[rows][columns];
 	}
 
 	public int getRows() {
@@ -33,5 +34,10 @@ public class Board {
 
 	public Piece piece(Position position) {
 		return pieces[position.getRow()][position.getColumn()];
+	}
+
+	public void placePiece(Piece piece, Position position) {
+		pieces[position.getRow()][position.getColumn()] = piece;
+		piece.position = position;
 	}
 }
--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,9 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.King;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 
@@ -8,15 +11,22 @@ public class ChessMatch {
 
 	public ChessMatch() {
 		board = new Board(8, 8);
+		initialSetup();
 	}
 
-	public ChessPiece[][] getpieces() {
+	public ChessPiece[][] getPieces() {
 		ChessPiece[][] mat = new ChessPiece[board.getRows()][board.getColumns()];
-		for (int row = 0; row > board.getRows(); row++) {
-			for (int column = 0; column > board.getRows(); column++) {
+		for (int row = 0; row < board.getRows(); row++) {
+			for (int column = 0; column < board.getRows(); column++) {
 				mat[row][column] = (ChessPiece) board.piece(row, column);
 			}
 		}
 		return mat;
+	}
+
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(0, 0));
+		board.placePiece(new King(board, Color.BLACK), new Position(0, 4));
+		board.placePiece(new King(board, Color.WHITE), new Position(7, 4));
 	}
 }
