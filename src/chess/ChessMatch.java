ec2eb7c595d378594b3e67ce7c241d2248cbdaee
Leandro Brotas <leandromacedo.tecnologia@gmail.com>
2019-05-17 08:54:17 -0300

    Pawn

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -8,6 +8,7 @@ import boardgame.Board;
 import boardgame.Piece;
 import boardgame.Position;
 import chess.pieces.King;
+import chess.pieces.Pawn;
 import chess.pieces.Rook;
 
 public class ChessMatch {
@@ -63,9 +64,11 @@ public class ChessMatch {
 		placeNewPiece('h', 7, new Rook(board, Color.WHITE));
 		placeNewPiece('d', 1, new Rook(board, Color.WHITE));
 		placeNewPiece('e', 1, new King(board, Color.WHITE));
+		placeNewPiece('b', 2, new Pawn(board, Color.WHITE));
 
 		placeNewPiece('b', 8, new Rook(board, Color.BLACK));
 		placeNewPiece('a', 8, new King(board, Color.BLACK));
+		placeNewPiece('a', 7, new Pawn(board, Color.BLACK));
 	}
 
 	public boolean[][] possibleMoves(ChessPosition sourcePosition) {
